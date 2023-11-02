626bccd3aed8e2108e24a1ae2ae5962bfcddce40
Lejoon <horosiewicz@gmail.com>
2023-11-02 16:46:37 +0100

    Updages

--- general_utils.py
+++ general_utils.py
@@ -13,24 +13,25 @@ def get_seconds_until(time_hour, time_minute):
         
     return int((target_time - now).total_seconds())
 
-async def retry_with_backoff(func, retries=5, base_delay=10.0, max_delay=120.0):
+async def retry_with_backoff(func, *args, retries=5, base_delay=1.0, max_delay=60.0):
     """
     Retry a function with exponential backoff.
 
     Parameters:
     - func: The function to retry. It should be a coroutine.
+    - *args: Arguments to pass to the function.
     - retries: The maximum number of retries before giving up. Default is 5.
     - base_delay: The base delay in seconds. Default is 1.0.
     - max_delay: The maximum delay in seconds. Default is 60.0.
     """
     for retry in range(retries):
         try:
-            return await func()
+            return await func(*args)
         except aiohttp.ClientConnectorError as e:
             if retry == retries - 1:  # If this was the last retry
                 raise  # Re-raise the last exception
             else:
                 # Calculate delay: base_delay * 2 ^ retry, but randomized to spread out the load
                 delay = min(max_delay, base_delay * 2 ** retry) * (0.5 + random.random())
-                print('Trying to reconnect in', delay, 'seconds...')
+                print('Awaiting retry in', delay, 'seconds.')
                 await asyncio.sleep(delay)
